4766e4dcd173e1dd3d8f02d489ba8e11f5b3f3c9
peterfarrell <pfarrell@greatbiztools.com>
2014-12-29 02:39:15 -0600

    - Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="django-health-check",
+    version='alpha',
+    author="Peter J. Farrell",
+    author_email="pfarrell@greatbiztools.com",
+    description="A simple way to auto discover AJAX_LOOKUP_CHANNELS in settings.py by looking in lookups.py for registered apps.",
+    license="MIT",
+    url="https://github.com/peterfarrell/django-ajax-selects-autodiscover",
+    packages=[
+        'ajax_selects_autodiscover',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
